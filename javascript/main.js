e1275ce3caf0a68b26966f70e3cc0ea2ad1f07d8
Syukri HSB <syukrihsb148@gmail.com>
2018-10-23 16:36:18 +0700

    finishing tugas client server 2 column

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,37 @@
 $(document).ready(function() {
   $("input[name='option']").on("change", function() {
     if (this.value == 1) {
-      $(".title.placeholder").html("Frontend");
+      $(".title.placeholder").html("Administrator Keamanan dan Jaringan");
     } else if (this.value == 2) {
-      $(".title.placeholder").html("Backend");
+      $(".title.placeholder").html("Analis dan Administrator Basisdata");
     } else if (this.value == 3) {
-      $(".title.placeholder").html("User Experience");
+      $(".title.placeholder").html("Software Engeenering");
     } else if (this.value == 4) {
-      $(".title.placeholder").html("Quality Assurance");
+      $(".title.placeholder").html("Vision Intelligent");
     } else {
-      $(".title.placeholder").html("Choose your Job...");
+      $(".title.placeholder").html("Pilih Peminatan.. !");
     }
   });
+
+
+  $("aside .nav a:nth-child(2)").on("click", function(e){
+    e.preventDefault();
+    $(".form").toggleClass("sorot");
+    $(".table.sorot").toggleClass("sorot");
+    $(".dropdown.sorot").toggleClass("sorot");
+  })
+
+  $("aside .nav a:nth-child(1)").on("click", function(e){
+    e.preventDefault();
+    $(".form.sorot").toggleClass("sorot");
+    $(".table").toggleClass("sorot");
+    $(".dropdown.sorot").toggleClass("sorot");
+  })
+
+   $("aside .nav a:nth-child(3)").on("click", function(e){
+    e.preventDefault();
+    $(".form.sorot").toggleClass("sorot");
+    $(".table.sorot").toggleClass("sorot");
+    $(".dropdown").toggleClass("sorot");
+  })
 });
